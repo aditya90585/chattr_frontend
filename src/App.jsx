48d03fd71dd460f9132162db6f00c69bdcc29495
Aditya Rajput <adityarajput81971@gmail.com>
2025-11-02 12:20:14 +0530

    redirect issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { toast, ToastContainer } from 'react-toastify'
 import './App.css'
 import { createBrowserRouter, Navigate, Outlet, RouterProvider, useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axiosInstance from './features/axioxInstance'
 import { useDispatch } from 'react-redux'
 import { login as LoginUser } from './redux/Slices/authSlices'
+import Loader from './components/Loader2'
 
 function App({ children }) {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
    try {
+    setLoading(true)
      axiosInstance.get("/user/getcurrentuser").then((res) => {
        if (res.data.success) {
          dispatch(LoginUser({ user: res.data.user }))
@@ -23,11 +26,15 @@ function App({ children }) {
     console.log(error.response.data,"error")
       toast.error(error?.response?.data?.message)
    }
+   finally{
+    setLoading(false)
+   }
 
 
   }, [])
 
 
+  if (loading)  return <Loader height={"screen"} width={"screen"} />
   return (
     <div className='min-h-screen flex flex-wrap content-between'>
       <div className='w-full block'>
--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -5,25 +5,39 @@ import Loader from "./Loader2"
 import { toast, ToastContainer } from "react-toastify"
 import axiosInstance from "../features/axioxInstance"
 import { useRef } from "react"
+import { login as LoginUser } from "../redux/Slices/authSlices"
+import { ToastContainer,toast } from "react-toastify"
+
 
 export const AuthLayout = ({ children }) => {
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
-  const authStatus = useSelector(state => state.auth.status)
-  const authref = useRef(authStatus)
+  const authStatus = useSelector(state => state?.auth?.status)
+  const dispatch = useDispatch()
   
-  useEffect(() => {
-    authref.current = authStatus
-  }, [authStatus])
+ 
 
 
   useEffect(() => {
-    setTimeout(() => {
-      if (!authref.current) {
-        toast.error("you are not log In")
-        navigate("/login")
-      }
-    }, 500);
+   
+      try {
+        setLoading(true)
+           axiosInstance.get("/user/getcurrentuser").then((res) => {
+             if (res.data.success) {
+               dispatch(LoginUser({ user: res.data.user }))
+              //  navigate("/")
+             }
+           }).catch((err)=>{
+            console.log(err)
+            navigate("/login")
+           })
+         } catch (error) {
+          console.log(error.response.data,"error")
+            toast.error(error?.response?.data?.message)
+         }
+         finally{
+          setLoading(false)
+         }
 
   }, [navigate, authStatus])
 
